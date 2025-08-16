ae018cea9e92e1f75f4a5b6b59c1dab14006d16a
jahan80 <jjjahannn@gmail.com>
2025-08-16 16:05:08 +0330

    com1

--- website1/urls.py
+++ website1/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from website1.views import *
 urlpatterns = [
     #jahantetst1
-    path('',home),
+    path('home',home),
     path('about', about),
     path('contact', contact),
 ]
