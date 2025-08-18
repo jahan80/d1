53f1098de7c71be9b578aa5d0f3912dadc326a35
jahan80 <jjjahannn@gmail.com>
2025-08-18 16:27:21 +0330

    static file

--- website1/views.py
+++ website1/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 def home(request):
     return  render(request, 'website/index.html')
 def about(request):
-    return render(request, 'website/about.html')
+    return render(request, 'website/../templates/about.html')
 def contact(request):
     return render(request, 'website/contact.html')
 
